7f8d292a6bfb105bc1537311ec208adc05a48a3f
Wenyi Tang <wenyi.tang@intel.com>
2023-07-11 09:26:46 +0800

    [fix] reshape strided_slice and interpolate

--- openvino2onnx/ops/strided_slice.py
+++ openvino2onnx/ops/strided_slice.py
@@ -14,11 +14,3 @@ class StridedSlice(Node):
     @property
     def type_name(self):
         return "Slice"
-
-    @property
-    def attributes(self):
-        if self.new_axis_mask or self.shrink_axis_mask or self.ellipsis_mask:
-            raise NotImplementedError(
-                "expect new_axis_mask, shrink_axis_mask, and ellipsis_mask to be empty"
-            )
-        return []
