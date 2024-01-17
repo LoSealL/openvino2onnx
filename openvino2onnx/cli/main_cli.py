3180b387488501414099a885b40d30e33000b1b4
Wenyi Tang <wenyi.tang@intel.com>
2024-01-17 10:27:58 +0800

    [dev] add a uniform api "transform"

--- openvino2onnx/cli/main_cli.py
+++ openvino2onnx/cli/main_cli.py
@@ -12,8 +12,7 @@ from pathlib import Path
 
 import onnx
 
-from openvino2onnx import build, ir_to_graph
-from openvino2onnx.legalize import legalize
+from openvino2onnx import transform
 
 
 def parse_args():
@@ -26,8 +25,8 @@ def parse_args():
         "--opset-version",
         "--opset",
         type=int,
-        choices=(9, 10, 11, 12, 13, 19),
-        default=13,
+        choices=set(range(11, 20)),
+        default=17,
         help="specify a version number of onnx opset",
     )
     parser.add_argument(
@@ -39,6 +38,7 @@ def parse_args():
 
 
 def main():
+    """Entry"""
     args = parse_args()
     model_url = args.model_url
     if model_url.startswith("omz://"):
@@ -47,14 +47,12 @@ def main():
 
         with tempfile.TemporaryDirectory() as tmpdir:
             model_url = download_from_omz_link(model_url, tmpdir)
-            graph = ir_to_graph(model_url, to_fp32=args.fp32)
+            model = transform(model_url, None, args.fp32, args.opset_version)
     else:
-        graph = ir_to_graph(model_url, args.model_bin, args.fp32)
-    graph = legalize(graph)
-    model = build(graph, version=args.opset_version)
+        model = transform(model_url, args.model_bin, args.fp32, args.opset_version)
     if args.output is None:
         out_file = Path(model_url).stem + ".onnx"
     else:
         out_file = Path(args.output).with_suffix(".onnx")
-    onnx.save(model, out_file)
-    print(f"onnx file saved at {out_file}")
+    onnx.save_model(model, out_file)
+    print(f"[I] onnx file saved at {out_file}")
